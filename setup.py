c1b95ac292e319f83f9762798a4f7e5dadacf2ea
ravishyadav112 <ravishkumar1242@gmail.com>
2025-07-30 14:24:33 +0530

    Add cookie-cutter template

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='A small ml project to detect whether a person is survived in titanic or what is the reason behind this',
+    author='ravish',
+    license='',
+)
