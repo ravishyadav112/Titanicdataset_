f2cbe03ae404079d7f73ebf43a07419142c50ceb
ravishyadav112 <ravishkumar1242@gmail.com>
2025-07-31 23:46:04 +0530

    Addig new model

--- src/model/model_building.py
+++ src/model/model_building.py
@@ -5,10 +5,10 @@ import json
 import pickle
 import logging
 from sklearn.preprocessing import StandardScaler
-from sklearn.ensemble import RandomForestClassifier
+from xgboost import XGBClassifier
 import yaml
 
-n_estimators = yaml.safe_load(open("params.yaml" , 'r'))['model_building']['n_estimators']
+
 
 # Setup logger
 logging.basicConfig(level=logging.INFO, format="%(asctime)s - %(levelname)s - %(message)s")
@@ -38,46 +38,39 @@ def scale_data(X: pd.DataFrame, test: pd.DataFrame) -> tuple[np.ndarray, np.ndar
         raise
 
 
-def train_model(X: np.ndarray, y: pd.Series) -> RandomForestClassifier:
+def train_model(X: np.ndarray, y: pd.Series) ->  XGBClassifier:
     logger.info("Training model...")
     try:
-        model = RandomForestClassifier(oob_score=True, random_state=42 , n_estimators=n_estimators)
+        model = XGBClassifier()
         model.fit(X, y)
-        logger.info(f"OOB Score: {model.oob_score_}")
+        logger.info("Model Trained ...")
         return model
     except Exception as e:
         logger.error(f"Error training model: {e}")
         raise
 
 
-def save_outputs(model: RandomForestClassifier, X_test_scaled: np.ndarray, metrics_path: str, model_path: str, test_data_path: str) -> None:
+def save_outputs(model: XGBClassifier, X_test_scaled: np.ndarray,  model_path: str, test_data_path: str) -> None:
     logger.info("Saving model and evaluation data...")
     try:
-        os.makedirs(metrics_path, exist_ok=True)
 
         # Save model
         with open(model_path, "wb") as f:
             pickle.dump(model, f)
 
         # Save test data
-        pd.DataFrame(X_test_scaled).to_csv(test_data_path, index=False)
+        pd.DataFrame(X_test_scaled).to_csv(os.path.join(test_data_path , "X_test_scaled"), index=False)
 
-        # Save metrics
-        with open(os.path.join(metrics_path, "metrics"), "w") as f:
-            json.dump({"oob_score": model.oob_score_}, f, indent=4)
-
-        logger.info(f"Model and metrics saved in {metrics_path}")
     except Exception as e:
-        logger.error(f"Error saving outputs: {e}")
-        raise
+        logging.error(f"Error in loading model{e}")
+
 
 
 def main():
     train_path = "./data/featured_data/train_data"
     test_path = "./data/featured_data/test_data"
     model_path = "./reports/model.pkl"
-    metrics_path = os.path.join("./models", "evaluation_data")
-    test_data_path = os.path.join("./data/interim", "X_test_scaled")
+    test_data_path ="./data/interim"
 
     try:
         train, test = load_data(train_path, test_path)
@@ -85,7 +78,7 @@ def main():
         X = train.iloc[:, 1:]
         X_train_scaled, X_test_scaled, _ = scale_data(X, test)
         model = train_model(X_train_scaled, y)
-        save_outputs(model, X_test_scaled, metrics_path, model_path, test_data_path)
+        save_outputs(model, X_test_scaled, model_path, test_data_path)
     except Exception as e:
         logger.error(f"Model building pipeline failed: {e}")
 
